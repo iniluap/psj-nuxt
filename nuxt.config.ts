c99acb0f61c4183480d0184052c956051f4bcff8
iniluap <pjjakubowska@gmail.com>
2026-09-09 15:41:39 +0200

    Enable timeline in devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,6 +66,6 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/a11y'
   ],
-  devtools: { enabled: true },
+  devtools: { enabled: true, timeline: { enabled: true } },
   compatibilityDate: '2024-04-03'
 });
