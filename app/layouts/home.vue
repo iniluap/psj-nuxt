7821b7011da73136017e96eabad5d2fb62d8958f
iniluap <pjjakubowska@gmail.com>
2026-09-09 18:37:21 +0200

    Specify two distinct layouts

---
+++ app/layouts/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="home-layout">
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+  .home-layout {
+    /* Specify minimum height for rows to reduce CLS */
+    grid-template-rows:
+      [header-start] minmax(48rem, min-content)
+      [content-start] min-content
+      [footer-start] minmax(6rem, min-content) [page-end];
+  }
+</style>
