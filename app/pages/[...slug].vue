34c169a74344d2bfd838de906b1269586665ebf7
iniluap <pjjakubowska@gmail.com>
2026-09-14 12:32:17 +0200

    Remove debugging

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -12,11 +12,6 @@
     return queryCollection('content').path(cleanPath).first();
   });
 
-  if (import.meta.client) {
-    console.log('Current route.path:', route.path);
-    console.log('Query result:', page.value);
-  }
-
   if (!page.value) {
     throw createError({
       statusCode: 404,
