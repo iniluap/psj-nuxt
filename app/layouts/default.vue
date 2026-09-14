d49d4cbf0d906f0c327137cd9772989c05a1c6b6
iniluap <pjjakubowska@gmail.com>
2026-09-14 12:34:01 +0200

    Make sure main spans the whole height

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -9,7 +9,7 @@
     padding: 14rem 0 0;
     /* Specify minimum height for rows to reduce CLS */
     grid-template-rows:
-      [content-start] min-content
+      [content-start] auto
       [footer-start] minmax(6rem, min-content) [page-end];
 
     @media screen and (min-width: 480px) {
--- app/layouts/home.vue
+++ app/layouts/home.vue
@@ -10,7 +10,7 @@
     /* Specify minimum height for rows to reduce CLS */
     grid-template-rows:
       [header-start] minmax(44rem, min-content)
-      [content-start] min-content
+      [content-start] auto
       [footer-start] minmax(6rem, min-content) [page-end];
   }
 </style>
