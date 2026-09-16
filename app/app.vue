383f90747fad20a75992738221eac604c0d22c5d
iniluap <pjjakubowska@gmail.com>
2026-09-16 16:07:01 +0200

    Add missing tags

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,28 @@
 <script>
   import '~/assets/css/global.css';
   import '~/assets/css/typography.css';
+
+  useSeoMeta({
+    title: 'Paulina Sędłak-Jakubowska',
+    description:
+      'Personal page of Paulina Sędłak-Jakubowska, senior frontend engineer and accessibility expert.',
+    ogTitle: 'Paulina Sędłak-Jakubowska',
+    ogDescription:
+      'Personal page of Paulina Sędłak-Jakubowska, senior frontend engineer and accessibility expert.',
+    ogUrl: 'https://paulina.s-j.me/'
+  });
+  useHead({
+    htmlAttrs: {
+      lang: 'en'
+    },
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon.png'
+      }
+    ]
+  });
 </script>
 
 <template>
